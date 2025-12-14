97fc060383ecaaae8b3c378fb31d5c775858d36b
ajayanilsree <ajayanilsree@gmail.com>
2025-12-15 03:16:51 +0530

    uploaded2

---
+++ c2.js
@@ -0,0 +1,41 @@
+/*this code prints hello world
+on the console of browser*/
+// console.log("Hello World");
+
+//Arithemetic Operators
+
+// let x=10;
+// let y="10";
+// console.log("x=y ",x==y);
+
+
+// let a=10;
+// let b="10";
+// console.log("a=b ",a===b);
+
+//Conditional Statements
+
+// let num=10;
+// if(num%2==0){
+//     console.log(num," is even.");
+// }
+// else{
+//     console.log(num," is odd.");
+// }
+
+// let age=45;
+// if(age<18){
+//     console.log("Minor");
+// }
+// else if(age>=18 && age<=60){
+//     console.log("Adult");
+// }
+// else{
+//     console.log("Senior");
+// }
+
+//Ternary Operator
+
+let age=9;
+let result=age>18?"Adult":"Minor";
+console.log(result);
---
+++ c2_ps2.js
@@ -0,0 +1,17 @@
+let mark=prompt("Enter your mark:");
+
+if (mark<=49){
+    console.log("F");
+}
+else if(mark>=50 && mark<=59){
+    console.log("D");
+}
+else if(mark>=60 && mark<=69){
+    console.log("C");
+}
+else if(mark>=70 && mark<=89){
+    console.log("B");
+}
+else{
+    console.log("A");
+}
