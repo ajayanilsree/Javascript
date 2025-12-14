d1f22c85bca65dce11045c0f683c42b1b575e0ab
ajayanilsree <ajayanilsree@gmail.com>
2025-12-14 19:47:42 +0530

    uploaded

---
+++ c1.js
@@ -0,0 +1,52 @@
+// fullName="Alex Karp";
+// console.log(fullName);
+// age=29;
+// console.log(age);
+
+// isFollow=true;
+// totalPrice=328;
+// console.log(isFollow);
+// console.log(totalPrice);
+
+// var fullName="Alex Karp";
+// var age=50;
+// var age=29;
+// console.log(fullName);
+// console.log(age);
+
+// let age=50;
+// age=29;
+// age=26;
+
+// console.log(age);
+
+// const PI=3.14;
+// console.log(PI);
+
+// {
+//     let age=29;
+//     console.log(age);
+// }
+// let age=50; 
+// console.log(age);
+
+
+// {
+//     let age=-60.555;
+//     console.log(typeof(age));
+// }
+
+const student={
+    fullName:"Alex Karp",
+    Age:50,
+    company:"Palantr Tech",
+    cgpa:8.9,
+    isPass:true,
+};
+
+// console.log(typeof(student));
+// console.log(student);
+console.log(student.fullName);
+console.log(student['fullName']);
+student.fullName="Jai Karp";
+console.log(student.fullName);
---
+++ ps1_cs1.js
@@ -0,0 +1,11 @@
+const product={
+    img:"pen.png",
+    fullName:"Parker Jottter Standarad CT Ball Pen",
+    color:"Black",
+    rating:4.0,
+    offer:true,
+    mrp:285,
+    sp:270,
+    discount:0.05,
+};
+console.log(product);
