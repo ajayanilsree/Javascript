f096fa94d25b4c04c84f7567aea13b587cce516d
ajayanilsree <ajayanilsree@gmail.com>
2025-12-19 23:40:34 +0530

    uploaded

---
+++ c6.js
@@ -0,0 +1,18 @@
+// let sub_head=document.getElementById("sub-head");
+// console.dir(sub_head);
+// console.log(sub_head);
+
+// let tech=document.getElementsByClassName("tech");
+// // console.log(tech);
+// console.dir(tech); 
+
+// let para=document.getElementsByTagName("p");
+// console.dir(para);
+
+// let para1=document.querySelector("p");
+// console.dir(para1);
+
+// let paraAll=document.querySelectorAll("p");
+// console.dir(paraAll);
+divInner=document.querySelector("div").innerText
+console.dir(divInner);
---
+++ c6_ps1.js
@@ -0,0 +1,6 @@
+boxes=document.querySelectorAll(".box");
+console.log(boxes);
+
+boxes[0].innerText="BBox 1";
+boxes[1].innerText="BBox 2";
+boxes[2].innerText="BBox 3";
