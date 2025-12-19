f1771aa0b8a6ce1b4dfc0a2524371287ce347d66
ajayanilsree <ajayanilsree@gmail.com>
2025-12-19 23:46:30 +0530

    uploaded

--- c6_ps1.js
+++ c6_ps1.js
@@ -1,6 +1,16 @@
 boxes=document.querySelectorAll(".box");
 console.log(boxes);
 
-boxes[0].innerText="BBox 1";
-boxes[1].innerText="BBox 2";
-boxes[2].innerText="BBox 3";
+let n=1;
+for(let box of boxes){
+    box.innerText=`BBox ${n}`;
+    n=n+1;
+}
+
+
+
+
+
+// boxes[0].innerText="BBox 1";
+// boxes[1].innerText="BBox 2";
+// boxes[2].innerText="BBox 3";
