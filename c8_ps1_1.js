0ca5386f86b02fbf86ae2c67714b4af8f12ade85
ajayanilsree <ajayanilsree@gmail.com>
2025-12-21 12:15:12 +0530

    uploaded

--- c8_ps1_1.js
+++ c8_ps1_1.js
@@ -1,5 +1,17 @@
 let body=document.querySelector("body");
 let btn=document.querySelector("#btn");
+// let mode=true;
+
+// btn.addEventListener("click",()=>{
+//     if(mode){
+//         body.classList.add("dark");
+//         mode=false;
+//     }
+//     else{
+//         body.classList.remove("dark");
+//         mode=true;
+//     }
+// });
 
 btn.addEventListener("click",()=>{
     body.classList.toggle("dark");
